e4ccb0033d31ccf91ff80d883affedaec3852725
Maciej Miklas <mac.miklas@gmail.com>
2014-01-21 21:52:30 +0100

    write history file - beginning....

--- src/main/java/org/cyclop/service/history/HistoryService.java
+++ src/main/java/org/cyclop/service/history/HistoryService.java
@@ -1,6 +1,8 @@
 package org.cyclop.service.history;
 
 import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 import org.cyclop.model.CqlQuery;
 import org.cyclop.model.QueryHistoryEntry;
 import org.cyclop.model.ServiceException;
@@ -14,7 +16,7 @@ public interface HistoryService {
     // TODO support time zone from browser
     void submitLastQuery(CqlQuery query, UserIdentifier userId)throws ServiceException;
     
-    List<QueryHistoryEntry> readHistory(UserIdentifier userId)throws ServiceException;
+    ImmutableList<QueryHistoryEntry> readHistory(UserIdentifier userId)throws ServiceException;
 
     boolean supported();
 }
--- src/main/java/org/cyclop/service/history/impl/HistoryServiceImpl.java
+++ src/main/java/org/cyclop/service/history/impl/HistoryServiceImpl.java
@@ -1,28 +1,28 @@
 package org.cyclop.service.history.impl;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Collections;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
-
+import com.google.common.collect.ImmutableList;
 import org.cyclop.common.AppConfig;
 import org.cyclop.model.CqlQuery;
 import org.cyclop.model.QueryHistoryEntry;
 import org.cyclop.model.ServiceException;
 import org.cyclop.model.UserIdentifier;
+import org.cyclop.service.converter.JsonMarshaller;
 import org.cyclop.service.history.HistoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Date;
 
 /**
  * @author Maciej Miklas
@@ -33,6 +33,9 @@ public class HistoryServiceImpl implements HistoryService {
 
     @Inject
     private AppConfig config;
+
+    @Inject
+    private JsonMarshaller jsonMarshaller;
 
     private boolean supported;
 
@@ -68,20 +71,48 @@ public class HistoryServiceImpl implements HistoryService {
 
     @Override
     public void submitLastQuery(CqlQuery query, UserIdentifier userId) throws ServiceException {
+        try (FileChannel channel = openForWrite(userId)) {
+            QueryHistoryEntry historyEntry = new QueryHistoryEntry(query, new Date());
+            String jsonText = jsonMarshaller.marshal(historyEntry) + "\n";
+            byte[] jsonByte = jsonText.getBytes(Charset.forName("UTF-8"));
+            ByteBuffer buf = ByteBuffer.allocate(jsonByte.length);
+            buf.flip();
+            channel.write(buf);
+        } catch (IOException | SecurityException e) {
+            throw new ServiceException(e.getMessage(), e);
+        }
     }
 
     @Override
-    public List<QueryHistoryEntry> readHistory(UserIdentifier userId) throws ServiceException {
-        try (FileChannel channel = openForRead(userId)) {
-            if (channel == null) {
-                return Collections.emptyList();
+    public ImmutableList<QueryHistoryEntry> readHistory(UserIdentifier userId) throws ServiceException {
+        ImmutableList.Builder<QueryHistoryEntry> respList = ImmutableList.builder();
+        try (FileInputStream in = openForRead(userId)) {
+            if (in == null) {
+                return ImmutableList.of();
+            }
+            Reader reader = new InputStreamReader(in, "UTF-8");
+            BufferedReader bufReader = new BufferedReader(reader);
+            for (int readLine = 1; readLine <= config.history.limit; readLine++) {
+                String line = bufReader.readLine();
+                LOG.debug("Reading history line({}): {}", readLine, line);
+                if (line == null) {
+                    break;
+                }
+                try {
+                    QueryHistoryEntry historyEntry = jsonMarshaller.unmarshal(QueryHistoryEntry.class, line);
+                    respList.add(historyEntry);
+                } catch (ServiceException e) {
+                    LOG.warn("Error reading history file: " + e.getMessage());
+                    LOG.debug(e.getMessage(), e);
+                }
             }
 
         } catch (IOException | SecurityException e) {
             throw new ServiceException(e.getMessage(), e);
         }
 
-        return null;
+        ImmutableList<QueryHistoryEntry> history = respList.build();
+        return history;
     }
 
     private FileChannel openForWrite(UserIdentifier userId) throws IOException {
@@ -92,17 +123,17 @@ public class HistoryServiceImpl implements HistoryService {
         return byteChannel;
     }
 
-    private FileChannel openForRead(UserIdentifier userId) throws IOException {
+    private FileInputStream openForRead(UserIdentifier userId) throws IOException {
         Path histPath = getPath(userId);
-        if (!Files.exists(histPath)) {
+        File file = histPath.toFile();
+        if (!file.exists() || !file.canRead()) {
             LOG.debug("History file not found: " + histPath);
             return null;
         }
-
-        FileChannel byteChannel = FileChannel.open(histPath, StandardOpenOption.READ);
-        FileLock lock = byteChannel.tryLock();
-        lock.close();
-        return byteChannel;
+        FileInputStream in = new FileInputStream(file);
+        FileChannel channel = in.getChannel();
+        channel.tryLock();
+        return in;
     }
 
     private Path getPath(UserIdentifier userId) {
